e001f789e854ff59a7933e8c907465425a43a043
Felix <thedarkomgf@gmail.com>
2022-07-21 12:38:19 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from 'express' //de ECMA script 6 equivaletne el require
+import ejs from 'ejs'
+import {dirname,join} from 'path'
+import { fileURLToPath } from 'url';
+
+import indexRoutes from './routes/index.js' 
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('views', join(__dirname,'views')); 
+
+app.set ('view engine','ejs');
+app.use(indexRoutes);
+
+app.use(express.static(join(__dirname,'public')))
+
+app.listen(3000);
+console.log ('Server is listening on port 3000');
+
+//Routes, son las rutas peticiones que hace un cliente al servidor
+//Un cliente hace una peticion de la ruta /contact, /help,/ informartion.
+//El clietne hace una peticion a una DIRECCION del servidor y este 
+//RESPONDE  con enviando un archivo y el navegador  se encarga de renderizarlo
